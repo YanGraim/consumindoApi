83506d8d9fc6217eeb08ad945e49ab441d19f8a1
YanGraim <yangraim@gmail.com>
2024-12-09 17:40:04 -0300

    cria função para consumir api

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 let url = 'https://sujeitoprogramador.com/rn-api/?api=posts';
 let listElement = document.querySelector("#app");
 let posts = [];
+
+
+function nutriApp() {
+    fetch(url)
+    .then((response) => response.json())
+    .then((json) => {
+        posts = json;
+
+        console.log(posts);
+    })
+}
+
+nutriApp();
